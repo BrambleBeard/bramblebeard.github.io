f99bb142fe858ef685a646837ad82e02a41f1fb3
Derek Nutile <dereknutile@gmail.com>
2017-06-07 16:08:08 -0700

    e/js Wired up clipboard.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -12,14 +12,14 @@ tinymce.init({
 var clipboard = new Clipboard('#copyToClipboard');
 
 clipboard.on('success', function(e) {
-    console.info('Action:', e.action);
-    console.info('Text:', e.text);
-    console.info('Trigger:', e.trigger);
+    console.info('Copied:', e.text);
 
     e.clearSelection();
+    return false;
 });
 
 clipboard.on('error', function(e) {
     console.error('Action:', e.action);
     console.error('Trigger:', e.trigger);
+    return false;
 });
